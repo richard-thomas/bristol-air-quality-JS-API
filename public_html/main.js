7aa045f3a48caada116a5c021b2018dc0610a034
richard-thomas <uk_richt@yahoo.co.uk>
2017-02-08 18:27:51 +0000

    Added home button. Broken mobile touch screen is because drop-down (non-visible) content pane steals events from map.

--- public_html/main.js
+++ public_html/main.js
@@ -5,6 +5,7 @@ require([
     "esri/map",
     "esri/arcgis/utils", // for "esri/arcgis/utils"::createMap method
     "esri/dijit/Legend",
+    "esri/dijit/HomeButton",
     "esri/dijit/BasemapGallery",
     "dojo/parser",
 
@@ -17,16 +18,23 @@ require([
         Map,
         arcgisUtils,
         Legend,
+        HomeButton,
         BasemapGallery,
         parser
     ) {
-        parser.parse();
+//        parser.parse();
 
         arcgisUtils.createMap(
             "0e2ff6096b2344028db18914af62d51b", // Bristol Air Quality webmap
             //"1bf4e342273541d989bda03d4ee9276a", // Basemap offline test4
             "mapDiv").then(function (response) {
         map = response.map;
+        
+        // Add "Home" button: zoom/pan to default extent
+        var home = new HomeButton({
+            map: map
+        }, "HomeButton");
+        home.startup();
 
         //add the basemap gallery, in this case we'll display maps from ArcGIS.com including bing maps
         var basemapGallery = new BasemapGallery({
@@ -38,12 +46,12 @@ require([
             console.log("basemap gallery error:  ", msg);
             });
 
-        var legend = new Legend({
-            map: map,
-            layerInfos:(arcgisUtils.getLegendLayers(response))
-        }, "legend");
-
-        legend.startup();                
+//        var legend = new Legend({
+//            map: map,
+//            layerInfos:(arcgisUtils.getLegendLayers(response))
+//        }, "legend");
+//
+//        legend.startup();                
     });
 });
 
