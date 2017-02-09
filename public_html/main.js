15022089c2788477b9215ca7e9ab4d60692015b2
richard-thomas <uk_richt@yahoo.co.uk>
2017-02-09 20:36:56 +0000

    Now with Legend/Basemap/LayerList in tabbed sidepane. Unfortunately splitter is useless on mobile. Would be much better with a ContentPane overlaid + display:none.

--- public_html/main.js
+++ public_html/main.js
@@ -7,9 +7,11 @@ require([
     "esri/dijit/Legend",
     "esri/dijit/HomeButton",
     "esri/dijit/BasemapGallery",
+    "esri/dijit/LayerList",
     "dojo/parser",
 
     "dijit/layout/BorderContainer",
+    "dijit/layout/TabContainer",
     "dijit/layout/ContentPane",
     "dijit/TitlePane",
     "dijit/form/Button",
@@ -20,10 +22,9 @@ require([
         Legend,
         HomeButton,
         BasemapGallery,
+        LayerList,
         parser
     ) {
-//        parser.parse();
-
         arcgisUtils.createMap(
             "0e2ff6096b2344028db18914af62d51b", // Bristol Air Quality webmap
             //"1bf4e342273541d989bda03d4ee9276a", // Basemap offline test4
@@ -46,12 +47,18 @@ require([
             console.log("basemap gallery error:  ", msg);
             });
 
-//        var legend = new Legend({
-//            map: map,
-//            layerInfos:(arcgisUtils.getLegendLayers(response))
-//        }, "legend");
-//
-//        legend.startup();                
+        var legend = new Legend({
+            map: map,
+            layerInfos:(arcgisUtils.getLegendLayers(response))
+        }, "legendDiv");
+        legend.startup(); 
+        
+        var layerList = new LayerList({
+            map: map,
+            layerInfos:(arcgisUtils.getLayerList(response))
+        }, "layerList");
+        layerList.startup(); 
+        
     });
 });
 
