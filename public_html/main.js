75a2fd2fd49bfa6e3a67150d183408aeb5a2dd67
richard-thomas <uk_richt@yahoo.co.uk>
2017-02-10 00:48:13 +0000

    Now with dedicated Legend/Basemap/LayerList buttons writing to a ContentPane overlaid + display:none/block. Bug: can't set z-index for close button

--- public_html/main.js
+++ public_html/main.js
@@ -9,6 +9,7 @@ require([
     "esri/dijit/BasemapGallery",
     "esri/dijit/LayerList",
     "dojo/parser",
+    "dojo/on",
 
     "dijit/layout/BorderContainer",
     "dijit/layout/TabContainer",
@@ -23,7 +24,8 @@ require([
         HomeButton,
         BasemapGallery,
         LayerList,
-        parser
+        parser,
+        on
     ) {
         arcgisUtils.createMap(
             "0e2ff6096b2344028db18914af62d51b", // Bristol Air Quality webmap
@@ -36,6 +38,39 @@ require([
             map: map
         }, "HomeButton");
         home.startup();
+        
+        // Handle presses on Basemap button
+        on(document.getElementById('btnBasemap'), "click", function() {
+            console.log('Basemap button was clicked!');
+            document.getElementById('sideBarR').style.display = 'block';
+            document.getElementById('basemapGallery').style.display = 'block';
+            document.getElementById('legend').style.display = 'none';
+            document.getElementById('layerList').style.display = 'none';
+        });
+
+        // Handle presses on Legend button
+        on(document.getElementById('btnLegend'), "click", function() {
+            console.log('Legend button was clicked!');
+            document.getElementById('sideBarR').style.display = 'block';
+            document.getElementById('legend').style.display = 'block';
+            document.getElementById('basemapGallery').style.display = 'none';
+            document.getElementById('layerList').style.display = 'none';
+        });
+
+        // Handle presses on Layers button
+        on(document.getElementById('btnLayers'), "click", function() {
+            console.log('Layers button was clicked!');
+            document.getElementById('sideBarR').style.display = 'block';
+            document.getElementById('layerList').style.display = 'block';
+            document.getElementById('basemapGallery').style.display = 'none';
+            document.getElementById('legend').style.display = 'none';
+        });
+
+        // Handle presses on Close button ('X')
+        on(document.getElementById('btnClose'), "click", function() {
+            console.log('Close button was clicked!');
+            document.getElementById('sideBarR').style.display = 'none';
+        });
 
         //add the basemap gallery, in this case we'll display maps from ArcGIS.com including bing maps
         var basemapGallery = new BasemapGallery({
@@ -50,7 +85,7 @@ require([
         var legend = new Legend({
             map: map,
             layerInfos:(arcgisUtils.getLegendLayers(response))
-        }, "legendDiv");
+        }, "legend");
         legend.startup(); 
         
         var layerList = new LayerList({
